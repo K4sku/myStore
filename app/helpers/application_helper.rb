3160538a1e1ff47597a9aa2957c6f147b8082ab7
K4sku <cezary.klos@gmail.com>
2019-09-01 09:54:04 +0200

    added login dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def page_title(title)
+    title += ' | ' if title.present?
+    title += 'Mydlo&Powidlo'
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
